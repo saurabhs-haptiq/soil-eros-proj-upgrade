9d236dd61cd0c6a6b0df8a46013b5fd32b641c54
saurabh.salvi <saurabh.salvi@haptiq.com>
2025-01-08 00:09:40 +0530

    added bg-img

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,19 @@
 import React from "react";
 import ImageComparisonForm from "./components/ImageComparisonForm";
 import Navbar from "./components/Navbar";
+import backgroundImage from './images/bg-img.jpg'; 
 
 function App() {
   return (
 
     
 
-    <div style={{ textAlign: "center", padding: "20px" }}>
+    <div style={{ textAlign: "center", padding: "20px", backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center', minHeight: '100vh'  }}>
       <Navbar />
       <h1>Soil Erosion Comparison Tool</h1>
       <ImageComparisonForm />
     </div>
 
-
-    /*
-    
-    <div style={{ display: "flex" }}>
-      <Navbar />
-      <div style={{ marginLeft: "220px", padding: "20px" }}>
-        <ImageComparisonForm />
-      </div>
-    </div>
-    
-    */
   );
 }
 
